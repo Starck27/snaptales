3f25c11054e4039ac3518012597b8e931f30cac2
Starck27 <adinataekwan27@gmail.com>
2025-05-28 10:56:29 +0700

    create and add skipToLink and transitionHelper functions

--- src/scripts/pages/app.js
+++ src/scripts/pages/app.js
@@ -1,43 +1,72 @@
-import routes from '../routes/routes';
-import { getActiveRoute } from '../routes/url-parser';
+import routes from "../routes/routes";
+import { getActiveRoute } from "../routes/url-parser";
+import { generateAuthenticatedNavigationListTemplate } from "../template";
+import { setupSkipToContent, transitionHelper } from "../utils";
 
 class App {
   #content = null;
   #drawerButton = null;
   #navigationDrawer = null;
+  #skipLinkButton = null;
 
-  constructor({ navigationDrawer, drawerButton, content }) {
+  constructor({ navigationDrawer, drawerButton, content, skipLinkButton }) {
     this.#content = content;
     this.#drawerButton = drawerButton;
     this.#navigationDrawer = navigationDrawer;
+    this.#skipLinkButton = skipLinkButton;
 
-    this._setupDrawer();
+    this.#init();
   }
 
-  _setupDrawer() {
-    this.#drawerButton.addEventListener('click', () => {
-      this.#navigationDrawer.classList.toggle('open');
+  #init() {
+    setupSkipToContent(this.#skipLinkButton, this.#content);
+
+    this.#setupDrawer();
+  }
+
+  #setupDrawer() {
+    this.#drawerButton.addEventListener("click", () => {
+      this.#navigationDrawer.classList.toggle("open");
     });
 
-    document.body.addEventListener('click', (event) => {
-      if (!this.#navigationDrawer.contains(event.target) && !this.#drawerButton.contains(event.target)) {
-        this.#navigationDrawer.classList.remove('open');
+    document.body.addEventListener("click", (event) => {
+      if (
+        !this.#navigationDrawer.contains(event.target) &&
+        !this.#drawerButton.contains(event.target)
+      ) {
+        this.#navigationDrawer.classList.remove("open");
       }
 
-      this.#navigationDrawer.querySelectorAll('a').forEach((link) => {
+      this.#navigationDrawer.querySelectorAll("a").forEach((link) => {
         if (link.contains(event.target)) {
-          this.#navigationDrawer.classList.remove('open');
+          this.#navigationDrawer.classList.remove("open");
         }
-      })
+      });
     });
+  }
+
+  #setupNavigationList() {
+    const navList = this.#navigationDrawer.children.namedItem("navlist");
+
+    navList.innerHTML = generateAuthenticatedNavigationListTemplate();
   }
 
   async renderPage() {
     const url = getActiveRoute();
     const page = routes[url];
 
-    this.#content.innerHTML = await page.render();
-    await page.afterRender();
+    const transition = transitionHelper({
+      updateDOM: async () => {
+        this.#content.innerHTML = await page.render();
+        page.afterRender();
+      },
+    });
+
+    transition.ready.catch(console.error);
+    transition.updateCallbackDone.then(() => {
+      scrollTo({ top: 0, behavior: "instant" });
+      this.#setupNavigationList();
+    });
   }
 }
 
--- src/scripts/utils/index.js
+++ src/scripts/utils/index.js
@@ -1,12 +1,32 @@
-export function showFormattedDate(date, locale = 'en-US', options = {}) {
+export function showFormattedDate(date, locale = "en-US", options = {}) {
   return new Date(date).toLocaleDateString(locale, {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
+    year: "numeric",
+    month: "long",
+    day: "numeric",
     ...options,
   });
 }
 
 export function sleep(time = 1000) {
   return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+export function setupSkipToContent(element, mainContent) {
+  element.addEventListener("click", () => mainContent.focus());
+}
+
+export function transitionHelper({ skipTransition = false, updateDOM }) {
+  if (skipTransition || !document.startViewTransition) {
+    const updateCallbackDone = Promise.resolve(updateDOM()).then(
+      () => undefined
+    );
+
+    return {
+      ready: Promise.reject(Error("View transitions unsupported")),
+      updateCallbackDone,
+      finished: updateCallbackDone,
+    };
+  }
+
+  return document.startViewTransition(updateDOM);
 }
