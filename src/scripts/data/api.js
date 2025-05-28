39481b6b0ef75fc78ec66394351725c657bcfbb0
Starck27 <adinataekwan27@gmail.com>
2025-05-28 13:44:27 +0700

    create getRegister edand getLogin function

--- src/scripts/data/api.js
+++ src/scripts/data/api.js
@@ -17,6 +17,48 @@ const ENDPOINTS = {
   UNSUBSCRIBE: `${CONFIG.BASE_URL}/notifications/unsubscribe`,
 };
 
+export async function getRegistered({ name, email, password }) {
+  if (password.length < 8) {
+    return {
+      error: true,
+      message: "Password setidaknya 8 karakter",
+      ok: false,
+    };
+  }
+
+  const data = JSON.stringify({ name, email, password });
+
+  const fetchResponse = await fetch(ENDPOINTS.REGISTER, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: data,
+  });
+
+  const json = await fetchResponse.json();
+
+  return {
+    ...json,
+    ok: fetchResponse.ok,
+  };
+}
+
+export async function getLogin({ email, password }) {
+  const data = JSON.stringify({ email, password });
+
+  const fetchResponse = await fetch(ENDPOINTS.LOGIN, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: data,
+  });
+
+  const json = await fetchResponse.json();
+
+  return {
+    ...json,
+    ok: fetchResponse.ok,
+  };
+}
+
 export async function getAllStories() {
   const accessToken = getAccessToken();
 
