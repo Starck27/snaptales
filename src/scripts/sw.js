0a0c2973511aa9ef5073e877e539ce65a6ea537c
Starck27 <adinataekwan27@gmail.com>
2025-06-09 18:23:55 +0700

    build runtime caching strategy

--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -1,7 +1,84 @@
+import { CacheableResponsePlugin } from "workbox-cacheable-response";
 import { precacheAndRoute } from "workbox-precaching";
+import { registerRoute } from "workbox-routing";
+import {
+  CacheFirst,
+  NetworkFirst,
+  StaleWhileRevalidate,
+} from "workbox-strategies";
+import CONFIG from "./config";
 
 // Do precaching
-precacheAndRoute(self.__WB_MANIFEST);
+const manifest = self.__WB_MANIFEST;
+precacheAndRoute(manifest);
+
+// Runtime caching
+registerRoute(
+  ({ url }) => {
+    return (
+      url.origin === "https://fonts.googleapis.com" ||
+      url.origin === "https://fonts.gstatic.com"
+    );
+  },
+  new CacheFirst({
+    cacheName: "google-fonts",
+  })
+);
+
+registerRoute(
+  ({ url }) => {
+    return (
+      url.origin === "https://cdnjs.cloudflare.com" ||
+      url.origin.includes("fontawesome")
+    );
+  },
+  new CacheFirst({
+    cacheName: "fontawesome",
+  })
+);
+
+registerRoute(
+  ({ url }) => {
+    return url.origin === "https://ui-avatars.com";
+  },
+  new CacheFirst({
+    cacheName: "avatars-api",
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
+
+registerRoute(
+  ({ request, url }) => {
+    const baseUrl = new URL(CONFIG.BASE_URL);
+    return baseUrl.origin === url.origin && request.destination !== "image";
+  },
+  new NetworkFirst({
+    cacheName: "snaptales-api",
+  })
+);
+
+registerRoute(
+  ({ request, url }) => {
+    const baseUrl = new URL(CONFIG.BASE_URL);
+    return baseUrl.origin === url.origin && request.destination === "image";
+  },
+  new StaleWhileRevalidate({
+    cacheName: "snaptales-api-images",
+  })
+);
+
+registerRoute(
+  ({ url }) => {
+    return url.origin.includes("maptiler");
+  },
+  new CacheFirst({
+    cacheName: "maptiler-api",
+  })
+);
 
 self.addEventListener("push", (event) => {
   console.log("Service worker pushing...");
